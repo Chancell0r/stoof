203b062b97b87fd37dfbb11ccd6cb53d74b9c5a6
Kohls <kohlsj@sea-kohlsj2-m.ds.ad.adp.com>
2015-04-29 10:17:38 -0700

    Add Primes Solution

---
+++ 1_countPrimes.cpp
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+
+void main(int argc, char** argv ){
+	int input = atoi(argv[0]);
+
+	int result = countPrimes(input);
+
+	cout << result;
+}
+
+int countPrimes(int n) {
+	int count = 0;
+	bool isPrime[n+1];
+    
+    for (int i = 2; i <= n; i++) {
+        isPrime[i] = true;
+    }
+    
+    for(int i = 2; i*i <= n; i++){
+        if(isPrime[i]){
+            for(int j = i; i*j <= n; j++){
+                isPrime[i*j] = false;
+            }
+        }
+    }
+    for(int i = 2; i <= n -1; i++){
+        if(isPrime[i]) count++;
+    }
+
+    return count;
+}
