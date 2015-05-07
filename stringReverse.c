b2aaf1a8585bd38e413842fcabb7283c2aff229e
Kohls <kohlsj@sea-kohlsj2-m.ds.ad.adp.com>
2015-05-07 09:12:26 -0700

    Add data checks, comments, and a performance enhancement

--- stringReverse.c
+++ stringReverse.c
@@ -1,3 +1,10 @@
+/**
+ *@author: Jeff Kohls
+ *
+ *
+ *Two different string reversals for Synapse's application process.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,26 +13,51 @@ void string_reverse1(char *string);
 int string_length(char* string);
 char *string_reverse2(const char *string);
 
-int main(int argv, char** argc){
+int main(int argc, char** argv){
 
-	string_reverse1(argc[1]);
-	printf("%s \n", argc[1]);
-	char* something = string_reverse2(argc[1]);
-	printf("%s \n", something);
-	free(something);
+
+    if(argc < 2){
+        printf("please provide valid string to be reversed\n");
+        return -1;
+    }
+
+    if(argc > 2){
+        printf("this program only accepts one parameter\n");
+        return -1;
+    }
+
+	string_reverse1(argv[1]);
+	printf("%s \n", argv[1]);
+
+	char* something = string_reverse2(argv[1]);
+
+
+    if(something){
+    	printf("%s \n", something);
+    	free(something);
+    }
+
+    return 0;
 }
 
+/*
+ *Reverse a string by manipulating the passed in char*
+ *
+ *@param *string - a string pointer that is to be reversed
+ */
 void string_reverse1(char *string)
 {
     int length = strlen(string);
+    if(length < 1 || !string){
+        return;
+    }
     int i = 0;
     char temp;
     char *end = string;
     char *begin = string;
 
-    for(i = 0; i < length - 1; i++){
-    	end++;
-    }
+    //point to the last letter of the string
+    end += ((length - 1) * sizeof(char));
 
     for(i = 0; i < (length / 2); i++){
     	temp = *end;
@@ -37,19 +69,32 @@ void string_reverse1(char *string)
     }
 }
 
+/*
+ *Reverse a string by copying the constant string coming in to a new character pointer and reversing the strings in
+ *  the reversed pointer.
+ *
+ *@param *string - a string pointer that is to be reversed but not changed directly since it is constant
+ */
 char* string_reverse2(const char *string){
 	int i;
 	int length = strlen(string);
 	char temp;
+
+    if(length < 1 || !string){
+        return NULL;
+    }
+
+    //allocate memory to the size of the string plus 1 to account for the trailing '\0'
 	char* stringCopy = (char*) malloc(sizeof(char) * (length + 1));
+
+    //copy the memory from one pointer to the new one
 	memcpy(stringCopy, string, length + 1);
+
 	char *end = stringCopy;
     char *begin = stringCopy;
 
-
-    for(i = 0; i < length - 1; i++){
-    	end++;
-    }
+    //point to last letter of the string
+    end += ((length - 1) * sizeof(char));
 
     for(i = 0; i < (length / 2); i++){
     	temp = *end;
